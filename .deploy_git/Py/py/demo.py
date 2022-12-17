532d7cf1d34fc004b6e9200387636d229cd2be91
xiaoxuan6 <1527736751@qq.com>
2022-12-17 13:40:19 +0800

    Add .deploy_git

---
+++ .deploy_git/Py/py/demo.py
@@ -0,0 +1,16 @@
+"""demo"""
+import os
+
+
+def prinff():
+    """内容输出"""
+    print('hello world')
+
+
+def path():
+    """打印当前路径"""
+    print(os.getcwd())
+
+
+if __name__ == '__main__':
+    prinff()
